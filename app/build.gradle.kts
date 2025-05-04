182db342355b20cd4c82411fe42a8590bac97e46
Filippo De Pretto <f.depretto@wishew.com>
2025-05-04 12:27:03 +0200

    update to targetSDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.filnik.goosegamekata"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.filnik.goosegamekata"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
