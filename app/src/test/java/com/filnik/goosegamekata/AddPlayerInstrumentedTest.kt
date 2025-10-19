758b73e370f402bd8e9d3c398bd0ac11f73f48e7
Filippo De Pretto <f.depretto@wishew.com>
2025-10-19 13:47:11 +0200

    fix unit tests

--- app/src/test/java/com/filnik/goosegamekata/AddPlayerInstrumentedTest.kt
+++ app/src/test/java/com/filnik/goosegamekata/AddPlayerInstrumentedTest.kt
@@ -1,10 +1,6 @@
 package com.filnik.goosegamekata
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
